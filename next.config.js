2e8246aa11182a18bd27b3d65f867d57841519ae
nancykaur11 <nancykaur1119@email.com>
2024-12-11 12:18:06 +0530

    third commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',           // The path you’re redirecting from
+        destination: '/home/page-three', // The path you’re redirecting to
+        permanent: true,       // Indicates if the redirect is permanent (301) or temporary (307)
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
